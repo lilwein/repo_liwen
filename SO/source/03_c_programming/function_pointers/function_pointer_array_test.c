fc3e6740769bd969e95ee147ca8445c64ea3a472
lilwein <zheng.1915940@studenti.uniroma1.it>
2023-05-25 13:32:29 +0200

    changeIndentation

--- SO/source/03_c_programming/function_pointers/function_pointer_array_test.c
+++ SO/source/03_c_programming/function_pointers/function_pointer_array_test.c
@@ -6,40 +6,41 @@ int run=1;
 typedef void (*ActionPtrFn)(int);
 
 void actionFn0(int ev) {
-  printf("Action 0, handling event %d\n", ev);
+	printf("Action 0, handling event %d\n", ev);
 }
 
 void actionFn1(int ev) {
-  printf("Action 1, handling event %d\n", ev);
+	printf("Action 1, handling event %d\n", ev);
 }
 
 void actionFnDefault(int ev) {
-  printf("default action, handling event %d\n", ev);
+	printf("default action, handling event %d\n", ev);
 }
 
 void actionFnQuit(int ev) {
-  printf("quitting %d\n", ev);
-  run = 0;
+	printf("quitting %d\n", ev);
+	run = 0;
 }
 
 // array of pointers to functions
 ActionPtrFn actions[MAX_ACTIONS];
 
 int main(int argc, char** argv) {
-  for (int i=0; i<MAX_ACTIONS; ++i)
-    actions[i]=actionFnDefault;
+	for (int i=0; i<MAX_ACTIONS; ++i)
+		actions[i]=actionFnDefault;
 
-  actions[0]=actionFn0;
-  actions[1]=actionFn1;
-  actions[8]=actionFn0;
-  actions[9]=actionFnQuit;
-  while(run) {
-    int v;
-    scanf("%d",&v);
-    if (v<0||v>=MAX_ACTIONS) {
-      printf("invalid index %d",v);
-      continue;
-    }
-    (*actions[v])(v);
-  }
+	actions[0]=actionFn0;
+	actions[1]=actionFn1;
+	actions[8]=actionFn0;
+	actions[9]=actionFnQuit;
+	
+	while(run) {
+		int v;
+		scanf("%d",&v);
+		if (v<0||v>=MAX_ACTIONS) {
+			printf("invalid index %d",v);
+			continue;
+		}
+		(*actions[v])(v);
+	}
 }
--- SO/source/03_c_programming/function_pointers/function_pointer_test.c
+++ SO/source/03_c_programming/function_pointers/function_pointer_test.c
@@ -12,25 +12,25 @@ void actionFn1();
 void actionFn2();
 
 void actionFn0(void) {
-  printf("handing  0\n");
-  action=actionFn1;
+	printf("handing  0\n");
+	action=actionFn1;
 }
 
 void actionFn1(void) {
-  printf("handing  1\n");
-  action=actionFn2;
+	printf("handing  1\n");
+	action=actionFn2;
 }
 
 void actionFn2(void) {
-  printf("handing  2\n");
-  action=actionFn0;
+	printf("handing  2\n");
+	action=actionFn0;
 }
 
 int main(int argc, char** argv) {
-  action=actionFn1;
-  while(1) {
-    int value;
-    getchar();
-    (*action)();
-  }
+	action=actionFn1;
+	while(1) {
+		int value;
+		getchar();
+		(*action)();
+	}
 }
