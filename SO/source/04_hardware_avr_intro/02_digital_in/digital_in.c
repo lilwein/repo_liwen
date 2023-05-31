ad2902c9ebdbdc34d968a6dd2e03e729129db560
lilwein <zheng.1915940@studenti.uniroma1.it>
2023-05-31 21:14:59 +0200

    changeIndentation

--- SO/source/04_hardware_avr_intro/02_digital_in/digital_in.c
+++ SO/source/04_hardware_avr_intro/02_digital_in/digital_in.c
@@ -5,24 +5,24 @@
 #include "../avr_common/uart.h" // this includes the printf and initializes it
 
 int main(void){
-  // this initializes the printf/uart thingies
-  printf_init(); 
+	// this initializes the printf/uart thingies
+	printf_init(); 
 
 
-  // we connect the switch to pin 12
-  // that is the bit 6 of port b
-  
-  const uint8_t mask=(1<<6);
-  // we configure the pin as input, clearing the bit 6
-  DDRB &= ~mask;
-  
-  // we enable pullup resistor on that pin
-  PORTB |= mask;
-  
-  while(1){
-    int key=(PINB&mask)==0; // we extract the bit value of the 6th bit
-    printf("switch %02x, %d\n", (int) PINB, key);
-    _delay_ms(200); // from delay.h, wait 1 sec
-  }
-  
+	// we connect the switch to pin 12
+	// that is the bit 6 of port b
+	
+	const uint8_t mask=(1<<6);
+	// we configure the pin as input, clearing the bit 6
+	DDRB &= ~mask;
+	
+	// we enable pullup resistor on that pin
+	PORTB |= mask;
+	
+	while(1){
+		int key=(PINB&mask)==0; // we extract the bit value of the 6th bit
+		printf("switch %02x, %d\n", (int) PINB, key);
+		_delay_ms(200); // from delay.h, wait 1 sec
+	}
+	
 }
