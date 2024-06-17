dad090462057b249901c115f954d1982031d3620
lilwein <zheng.1915940@studenti.uniroma1.it>
2024-06-17 17:07:52 +0200

    fdi2_ex8_comments

--- FDI2/Esercitazioni/8_Grafi/C-aux/graph.h
+++ FDI2/Esercitazioni/8_Grafi/C-aux/graph.h
@@ -24,54 +24,35 @@ typedef struct graph {
 	graph_prop* properties;
 } graph;
 
-/**
-Create a new graph.
-*/
+
+// Create a new graph.
 graph * graph_new();
 
-/**
-Returns the list of vertices of g.
-*/
+// Returns the list of vertices of g.
 linked_list * graph_get_nodes(graph * g);
 
-/**
-Returns a list containing all the outgoing edges from n in g.
-*/
+// Returns a list containing all the outgoing edges from n in g.
 linked_list * graph_get_neighbors(graph * g, graph_node * n);
 
-/**
-Adds and returns a new node with the value given in input to the graph g.
-*/
+// Adds and returns a new node with the value given in input to the graph g.
 graph_node * graph_add_node(graph * g, void * value);
 
-/**
-Adds an edge connecting from->to in the directed graph g.
-*/
+// Adds an edge connecting from->to in the directed graph g.
 void graph_add_edge(graph * g, graph_node * from, graph_node * to);
 
-/**
-Returns a pointer to the value of the node n.
-*/
+// Returns a pointer to the value of the node n.
 void * graph_get_node_value(graph_node * n);
 
-/**
-Removes edge v1->v2 from the graph g.
-*/
+// Removes edge v1->v2 from the graph g.
 void graph_remove_edge(graph* g, graph_node* v1, graph_node* v2);
 
-/**
-Removes the node v from the graph g.
-*/
+// Removes he node v from the graph g.
 void graph_remove_node(graph* g, graph_node* v);
 
-/**
-Prints a representation of the graph g on stdout.
-*/
+// Prints a representation of the graph g on stdout.
 void graph_print(graph* g);
 
-/**
-Deletes the graph g.
-*/
+// Deletes the graph g.
 void graph_delete(graph * g);
 
 
